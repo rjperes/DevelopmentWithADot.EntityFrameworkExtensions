1e33a55f3ea57947f1bebf2d8a87b0e07ea7f31f
Ricardo Peres <rjperes@hotmail.com>
2013-06-18 17:07:28 +0100

    Added BulkInsert and ExecuteQuery extension methods

--- DevelopmentWithADot.EntityFrameworkExtensions/DbContextExtensions.cs
+++ DevelopmentWithADot.EntityFrameworkExtensions/DbContextExtensions.cs
@@ -1,13 +1,75 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Metadata.Edm;
+using System.Data.Objects;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text.RegularExpressions;
 
 namespace DevelopmentWithADot.EntityFrameworkExtensions
 {
 	public static class DbContextExtensions
 	{
+		public static void BulkInsert<T>(this DbContext ctx, IEnumerable<T> items)
+		{
+			using (SqlBulkCopy bcp = new SqlBulkCopy(ctx.Database.Connection as SqlConnection))
+			{
+				DataTable table = new DataTable();
+				IEnumerable<PropertyDescriptor> columns = TypeDescriptor.GetProperties(typeof(T))
+					.OfType<PropertyDescriptor>()
+					.Where(x => (typeof(IEnumerable).IsAssignableFrom(x.PropertyType) == false) || x.PropertyType == typeof(String))
+					.Where(x => x.IsReadOnly == false);
+				ObjectContext octx = (ctx as IObjectContextAdapter).ObjectContext;
+
+				foreach (PropertyDescriptor column in columns)
+				{
+					table.Columns.Add(column.Name, column.PropertyType);
+				}
+
+				table.BeginLoadData();
+
+				EntitySetBase entitySet = octx.MetadataWorkspace.GetItemCollection(DataSpace.SSpace)
+				.GetItems<EntityContainer>()
+				.Single()
+				.BaseEntitySets
+				.Where(x => x.Name == typeof(T).Name)
+				.Single();
+
+				bcp.DestinationTableName = entitySet.MetadataProperties["Table"].Value.ToString(); 
+
+				foreach (T item in items)
+				{
+					DataRow row = table.NewRow();
+					table.Rows.Add(row);
+
+					foreach (PropertyDescriptor column in columns)
+					{
+						row[column.Name] = column.GetValue(item);
+					}
+				}
+
+				table.EndLoadData();
+
+				bcp.WriteToServer(table);
+			}
+		}
+
+		public static IEnumerable<T> ExecuteQuery<T>(this DbContext ctx, String entitySql, params Object [] parameters)
+		{
+			ObjectContext octx = (ctx as IObjectContextAdapter).ObjectContext;
+
+			entitySql = Regex.Replace(entitySql, "{(?<x>\\d+)}", "@p${x}");
+
+			return (octx.CreateQuery<T>(entitySql, parameters.Select((element, index) => new ObjectParameter(String.Format("p{0}", index), element)).ToArray()).ToList());
+		}
+
 		public static Object Load(this DbContext context, Type type, params Object[] keyValues)
 		{
 			return (context.Set(type).Find(keyValues));
